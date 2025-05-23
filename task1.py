cf0b1ff8818f02c86e878e59b5e6df76668b2542
Serhii <158744795+BurglachOK@users.noreply.github.com>
2025-05-23 22:55:53 +0200

    task1 done

--- task1.py
+++ task1.py
@@ -1,22 +1,37 @@
-import Queue
+from queue import Queue
+
 
 # Створити чергу заявок
 queue = Queue()
+n = 0
 
-def generate_request():
-    pass
+
+def generate_request(num):
+    global n
     # Створити нову заявку
-    # Додати заявку до черги
+    for i in range(num):
+        n += 1
+        # Додати заявку до черги
+        queue.put(f'Заявка №{n}')
 
-def process_request():
-    pass
-    # Якщо черга не пуста:
-    #     Видалити заявку з черги
-    #     Обробити заявку
-    # В іншому випадку::
-    #     Вивести повідомлення, що черга пуста
+
+def process_request(queue):
+    while not queue.empty():
+            # Витягти заявку з черги
+            request = queue.get()
+            # Обробити заявку
+            print(f'Обробка {request}')
+    # Вивести повідомлення, що черга пуста
+    print('Черга пуста')
+    global n
+    n = 0
+
 
 # Головний цикл програми:
-#     Поки користувач не вийде з програми:
-#         Виконати generate_request() для створення нових заявок
-#         Виконати process_request() для обробки заявок
+# Поки користувач не вийде з програми:
+while user_input := input("Введіть кількість заявок для генерації: "):
+    num = int(user_input)
+    # Виконати generate_request() для створення нових заявок
+    generate_request(num)
+    # Виконати process_request() для обробки заявок
+    process_request(queue)
